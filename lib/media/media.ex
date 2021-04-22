9657d1cd8ea05eff53096eb2520d8dede0d1aa5a
Zaher <z.hassan@eweev.com>
2021-04-22 12:35:13 +0300

    Schema validations for MongoDB

--- lib/media/media.ex
+++ lib/media/media.ex
@@ -3,19 +3,24 @@ defmodule Media.Schema do
     This is the media schema model.
     It represents the media properties and their types.
   """
+  @common_metadata ~w(platform url size type filename)a
+  @metadata_per_type %{"video" => ~w(duration)a, "podcast" => ~w(duration)a}
   use Ecto.Schema
   import Ecto.Changeset
-
-  @fields ~w(title author tracker tags type metadata)a
+  # alias Media.Schema
+  @fields ~w(title author contents_used tags type locked_status private_status files)a
   # @derive {Jason.Encoder, only: @fields}
   schema "media" do
     field(:tags, {:array, :string})
     field(:title, :string)
     field(:author, :string)
-    field(:tracker, :integer)
+    field(:contents_used, {:array, :string})
+    ## the map keys will be platforms
+    ## [%{"size"=> 1_000, url=> "http://image.com/image/1", "filename"=> "image/1"}]
+    field(:files, {:array, :map})
     field(:type, :string)
-    field(:metadata, :map)
-    # field(:required_fields, {:array, :string})
+    field(:locked_status, :string, default: "locked")
+    field(:private_status, :string, dedfault: "private")
 
     timestamps()
   end
@@ -23,5 +28,86 @@ defmodule Media.Schema do
   def changeset(media, attrs) do
     media
     |> cast(attrs, @fields)
+    |> validate_inclusion(:locked_status, ["locked", "unlocked"])
+    |> validate_required(:author)
+    |> validate_required(:type)
+    |> validate_inclusion(:private_status, ["public", "private"])
+    |> validate_inclusion(:type, ["image", "video", "document", "podcast"])
+    |> validate_files(attrs |> Map.get(:files) || attrs |> Map.get("files"))
+
+    # changeset
+    # |> cast_embed(:files,
+    #   with: {Schema, :files_changeset, [get_field(changeset, :type)]}
+    # )
+  end
+
+  def validate_files(%Ecto.Changeset{valid?: false} = changeset, _files), do: changeset
+
+  def validate_files(changeset, files) when is_nil(files), do: changeset
+
+  def validate_files(changeset, files) when files == [], do: changeset
+
+  def validate_files(changeset, files) do
+    type = changeset |> get_field(:type)
+
+    files
+    |> validate_contents(type)
+    |> case do
+      {:error, error} -> changeset |> add_error(:files, error)
+      _files -> changeset
+    end
+  end
+
+  def validate_contents(files, type) do
+    {_valid, result} =
+      files
+      |> Enum.reduce(
+        {true, []},
+        fn
+          file, {true, _acc} ->
+            {valid, content} = validate_content(file, type)
+            {true and valid, content}
+
+          _file, {false, result} ->
+            {false, result}
+        end
+      )
+
+    result
+  end
+
+  def validate_content(file, _type) when file == %{}, do: {:error, "Do not provide empty files"}
+
+  def validate_content(file, type) when is_map(file) do
+    {:ok, file} =
+      file
+      |> Morphix.atomorphify()
+
+    with {:keys, true} <-
+           {:keys,
+            file
+            |> Map.keys()
+            |> Enum.all?(&(&1 in (@common_metadata ++ Map.get(@metadata_per_type, type, []))))},
+         {:platforms, true} <- {:platforms, Map.get(file, :platform) in available_platforms()} do
+      {true, file}
+    else
+      {:keys, false} ->
+        {false,
+         {:error,
+          "You provided invalid keys for the files. Please Make sure to provide the supported metadata on files"}}
+
+      {:platforms, false} ->
+        {false, {:error, "Invalid Platform, Make sure to provide an existing platform"}}
+    end
+  end
+
+  def validate_content(_files, _type),
+    do: {:error, "The format of the files sent is not supported, please send a list of files"}
+
+  def mapify(res) when is_struct(res), do: Map.from_struct(res)
+  def mapify(res), do: res
+
+  def available_platforms do
+    ["desktop", "mobile"]
   end
 end
